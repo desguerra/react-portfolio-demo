213d8fde4cf930e2ed1c6331682b095b05e40011
desguerra <ddesguerra@ucdavis.edu>
2022-11-17 17:53:09 -0800

    add About component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Footer from './components/Footer';
 import Contact from './components/Contact';
 import Resume from './components/Resume';
 import Portfolio from './components/Portfolio';
+import About from './components/About';
 
 function App() {
   return (
     <div>
       <Header />
       <main>
+        <About />
         <Portfolio />
         <Contact />
         <Resume />
---
+++ src/components/About/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section className='container-fluid'>
+      <div className='mx-5 py-2'>
+        <h2>about me</h2>
+      </div>
+    </section>
+  );
+}
+
+export default About;
