6016a1ab50905e4766de0e57fa79fe73714855c5
desguerra <ddesguerra@ucdavis.edu>
2022-11-17 18:07:51 -0800

    finish About UI and update css for rest of components

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,23 +5,27 @@ function Contact() {
     <section className='container-fluid'>
       <div className='mx-5 py-2'>
         <h2>contact me</h2>
-        <form id='contact-form'>
-        <div>
-          <label htmlFor='name' className='form-label'>name:</label>
-          <input type='text' name='name' className='form-control'/>
-        </div>
-        <div>
-          <label htmlFor='email' className='form-label'>email address:</label>
-          <input type='email' name='email' className='form-control'/>
-        </div>
-        <div>
-          <label htmlFor='message' className='form-label'>message:</label>
-          <textarea name='message' rows='5' className='form-control'/>
-        </div>
 
-        <button className='btn-sub' type='submit'>submit</button>
+        <div className='p-4'>
+          <form id='contact-form'>
+          <div>
+            <label htmlFor='name' className='form-label'>name:</label>
+            <input type='text' name='name' className='form-control'/>
+          </div>
+          <div>
+            <label htmlFor='email' className='form-label'>email address:</label>
+            <input type='email' name='email' className='form-control'/>
+          </div>
+          <div>
+            <label htmlFor='message' className='form-label'>message:</label>
+            <textarea name='message' rows='5' className='form-control'/>
+          </div>
 
-        </form>
+          <button className='btn-sub' type='submit'>submit</button>
+
+          </form>
+        </div>
+        
       </div>
     </section>
   );
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -6,35 +6,39 @@ function Resume() {
       <div className='mx-5 py-2'>
         <h2>resume</h2>
 
-        <p>
-          Download my <a href='/'>resume</a>!
-        </p>
+        <div className='p-4'>
+          <p>
+            Download my <a href='/'>resume</a>!
+          </p>
 
-        <div>
-          <h3>front-end proficiencies</h3>
-          <ul>
-            <li>React</li>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JavaScript</li>
-            <li>jQuery</li>
-            <li>responsive web design</li>
-            <li>Bootstrap</li>
-          </ul>
+          <div>
+            <h3>front-end proficiencies</h3>
+            <ul>
+              <li>React</li>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>jQuery</li>
+              <li>responsive web design</li>
+              <li>Bootstrap</li>
+            </ul>
+          </div>
+
+          <div>
+            <h3>back-end proficiencies</h3>
+            <ul>
+              <li>APIs</li>
+              <li>Node JS</li>
+              <li>Experss.js</li>
+              <li>MySQL, Sequelize</li>
+              <li>MongoDB, Mongoose</li>
+              <li>REST</li>
+              <li>GraphQL</li>
+            </ul>
+          </div>
+
         </div>
 
-        <div>
-          <h3>back-end proficiencies</h3>
-          <ul>
-            <li>APIs</li>
-            <li>Node JS</li>
-            <li>Experss.js</li>
-            <li>MySQL, Sequelize</li>
-            <li>MongoDB, Mongoose</li>
-            <li>REST</li>
-            <li>GraphQL</li>
-          </ul>
-        </div>
       </div>
     </section>
   );
